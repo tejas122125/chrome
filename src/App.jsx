c55e92860ee457ff9805f86e9eb65635b5133e38
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-17 23:45:29 +0530

    tejas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-
+import "./index.css"
 function App() {
 
   return (
     <>
       
-      <div className="bg-red-700 h-screen w-screen">
-moinuiiudchuidcghuyjdbc
+      <div className="main">
+        <h2 className="header">Easy Code Snippet</h2>
+        <div className="screenshot" > <h4>Take ScreenShot</h4>
+        <button className="screenshotbutton" >Take</button>
+        </div>
+        <div className="clipboard">
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+          <div className="clipboardcontent">
+            <p className="copycontent">punit jinda bad</p>
+            <button className="copybutton">copy</button>
+          </div>
+        </div>
+
+
       </div>
     </>
   )

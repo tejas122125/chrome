d3f45f36953b696a825f5b71c73d65faa90ed7ff
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-26 23:33:04 +0530

    tejas

--- imagetest.py
+++ imagetest.py
@@ -6,7 +6,7 @@ model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 processor = AutoProcessor.from_pretrained("Salesforce/blip-vqa-base")
 
 url = "https://imgs.search.brave.com/mhkPmLdV7xh36KCFk26kO7LUBYBYIQ4RU1RGHBUWdIk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Ym9yZWRwYW5kYS5j/b20vYmxvZy93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMi8wNi9j/b25mdXNpbmctaW1h/Z2VzLXJlcXVpcmUt/bW9yZS1jb250ZXh0/LTItNjJiYTlkY2Jk/NDlhNF9fNzAwLmpw/Zw"
-# image = Image.open(requests.get(url, stream=True).raw)
+image = Image.open(requests.get(url, stream=True).raw)
 
 
 # inference

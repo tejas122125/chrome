df96c0b426eca73d049eaccfcec52a5488cd4d1b
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-26 23:33:58 +0530

    tejas

--- imagetest.py
+++ imagetest.py
@@ -2,7 +2,7 @@ from PIL import Image
 import requests
 from transformers import AutoProcessor, BlipForQuestionAnswering
 
-model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
+# model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
 processor = AutoProcessor.from_pretrained("Salesforce/blip-vqa-base")
 
 url = "https://imgs.search.brave.com/mhkPmLdV7xh36KCFk26kO7LUBYBYIQ4RU1RGHBUWdIk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/Ym9yZWRwYW5kYS5j/b20vYmxvZy93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMi8wNi9j/b25mdXNpbmctaW1h/Z2VzLXJlcXVpcmUt/bW9yZS1jb250ZXh0/LTItNjJiYTlkY2Jk/NDlhNF9fNzAwLmpw/Zw"

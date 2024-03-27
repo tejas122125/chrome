1d5344310e118298ee07d6f33cf058ffb509d2f6
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-27 23:30:14 +0530

    tejs

--- pdfchat.py
+++ pdfchat.py
@@ -2,9 +2,14 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains import ConversationChain
+
+
+
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 def get_pdf_text(pdf_docs):
@@ -36,4 +41,6 @@ def get_vectorstore(text_chunks):
 
 def get_conversational_chain():
     llm = ChatOpenAI(temperature=0.2)
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     

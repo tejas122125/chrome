1b1f8fb9089ecee3a42e2eb73c0e43ba5d7ac9c2
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-28 09:31:20 +0530

    tejs

--- chatwithwebsite.py
+++ chatwithwebsite.py
@@ -32,6 +32,19 @@ def data_loader(urls):
     for url in urls :
         loader = UnstructuredURLLoader(urls=url)
 
+
+
+        
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
         
 
 def main():
